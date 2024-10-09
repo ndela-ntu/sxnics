56d9b29836eb7f5fef7f5702544642390911af75
ndela-ntu <ntulilindelani4@gmail.com>
2024-10-09 15:49:57 +0200

    removed true

--- app/api/ListWebhooks/route.ts
+++ app/api/ListWebhooks/route.ts
@@ -36,7 +36,7 @@ export async function GET() {
     //   console.log(result.message);
     // });
 
-    return NextResponse.json({ hookExists: true, subscriptions });
+    return NextResponse.json({ hookExists, subscriptions });
   } catch (error) {
     console.error(error);
   }
