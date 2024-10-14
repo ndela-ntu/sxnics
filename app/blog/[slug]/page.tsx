10025f93d0edd128fdefa936b3ffb62f16f5634c
ndela-ntu <ntulilindelani4@gmail.com>
2024-10-14 16:13:40 +0200

    minor fix

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,7 +1,5 @@
 import Post from "@/components/blog/Post";
 import client from "@/lib/contentful";
-import BlogPost from "@/models/BlogPost";
-import connectMongo from "@/utils/ConnectMongo";
 import { notFound } from "next/navigation";
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
 import { BLOCKS, MARKS } from '@contentful/rich-text-types'
