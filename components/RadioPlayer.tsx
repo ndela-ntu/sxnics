52c937a009244200a80824b6d401a2f55dad1163
ndela-ntu <ntulilindelani4@gmail.com>
2024-10-15 14:06:38 +0200

    error boundary and disconnected socket

--- components/RadioPlayer.tsx
+++ components/RadioPlayer.tsx
@@ -20,97 +20,11 @@ const RadioPlayer: React.FC = () => {
 
   useEffect(() => {
     const audioElement = audioRef.current;
-
-    if (audioElement) {
-      // Create a new MediaSource
-      const mediaSource = new MediaSource();
-      mediaSourceRef.current = mediaSource;
-
-      // Attach the MediaSource to the audio element
-      audioElement.src = URL.createObjectURL(mediaSource);
-
-      mediaSource.addEventListener("sourceopen", handleSourceOpen);
-      mediaSource.addEventListener("sourceended", handleSourceEnd);
-      mediaSource.addEventListener("sourceclose", handleSourceClose);
-    }
-
-    // Connect to Socket.IO server
-    const socket = io("http://localhost:5000");
-
-    socket.on("audioChunk", (chunk: ArrayBuffer) => {
-      appendAudioChunk(chunk);
-    });
-
-    socket.on("nowPlaying", (currentTrack: { fileName: string }) => {
-      const displayName = currentTrack.fileName.replace(/\.mp3$/, "");
-      setNowPlaying(displayName);
-    });
-
-
     const checkScreenSize = () => {
       setIsSmallScreen(window.innerWidth < 1024); // 1024px is the default breakpoint for 'lg' in Tailwind
     };
-
     checkScreenSize();
-    window.addEventListener("resize", checkScreenSize);
-
-    return () => {
-      socket.disconnect();
-      window.removeEventListener("resize", checkScreenSize);
-      if (mediaSourceRef.current) {
-        mediaSourceRef.current.removeEventListener(
-          "sourceopen",
-          handleSourceOpen
-        );
-        mediaSourceRef.current.removeEventListener(
-          "sourceended",
-          handleSourceEnd
-        );
-        mediaSourceRef.current.removeEventListener(
-          "sourceclose",
-          handleSourceClose
-        );
-      }
-    };
   }, []);
-
-  const handleSourceOpen = () => {
-    const mediaSource = mediaSourceRef.current;
-    if (mediaSource && mediaSource.sourceBuffers.length === 0) {
-      const mimeType = "audio/mpeg"; // Adjust MIME type based on your audio format
-      try {
-        const sourceBuffer = mediaSource.addSourceBuffer(mimeType);
-        sourceBufferRef.current = sourceBuffer;
-
-        // Handle SourceBuffer events
-        sourceBuffer.addEventListener("updateend", () => {});
-      } catch (e) {
-        console.error("Error creating SourceBuffer:", e);
-      }
-    }
-  };
-
-  const handleSourceEnd = () => {
-    console.log("MediaSource ended");
-  };
-
-  const handleSourceClose = () => {
-    console.log("MediaSource closed");
-  };
-
-  const appendAudioChunk = (chunk: ArrayBuffer) => {
-    const sourceBuffer = sourceBufferRef.current;
-
-    if (sourceBuffer && !sourceBuffer.updating) {
-      try {
-        sourceBuffer.appendBuffer(new Uint8Array(chunk));
-      } catch (error) {
-        console.error("Error appending audio chunk:", error);
-      }
-    } else {
-      console.log("SourceBuffer is updating or unavailable.");
-    }
-  };
 
   const handlePlayPause = () => {
     const audio = audioRef.current;
