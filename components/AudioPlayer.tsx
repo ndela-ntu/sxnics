ba701e20a9f6c886966e40eb59357ba16ada6808
ndela-ntu <ntulilindelani4@gmail.com>
2024-10-16 19:01:48 +0200

    loading indicator to audio player

--- components/AudioPlayer.tsx
+++ components/AudioPlayer.tsx
@@ -1,8 +1,8 @@
+"use client";
 import React, { useRef, useState, useEffect } from "react";
-import { Play, Pause } from "lucide-react";
+import { Play, Pause, Loader } from "lucide-react";
 import Image from "next/image";
 import { IEpisode } from "@/models/Episode";
-import { FaArrowUp } from "react-icons/fa";
 import { MdOutlineKeyboardArrowUp } from "react-icons/md";
 
 export default function AudioPlayer({
@@ -17,10 +17,10 @@ export default function AudioPlayer({
   onXClick: () => void;
 }) {
   const audioRef = useRef<HTMLAudioElement>(null);
-  // const [isPlaying, setIsPlaying] = useState(episode.isPlaying);
   const [currentTime, setCurrentTime] = useState(0);
   const [duration, setDuration] = useState(0);
   const [isMoreOpen, setIsMoreOpen] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const audio = audioRef.current;
@@ -29,18 +29,28 @@ export default function AudioPlayer({
     const setAudioData = () => {
       setDuration(audio.duration);
       setCurrentTime(audio.currentTime);
+      setIsLoading(false);
     };
 
     const setAudioTime = () => setCurrentTime(audio.currentTime);
 
+    const handleEnded = () => {
+      onTogglePlay(false);
+      if (audio) {
+        audio.currentTime = 0;
+      }
+    };
+
     audio.addEventListener("loadeddata", setAudioData);
     audio.addEventListener("timeupdate", setAudioTime);
+    audio.addEventListener("ended", handleEnded);
 
     return () => {
       audio.removeEventListener("loadeddata", setAudioData);
       audio.removeEventListener("timeupdate", setAudioTime);
+      audio.removeEventListener("ended", handleEnded);
     };
-  }, [episode]);
+  }, [episode, onTogglePlay]);
 
   useEffect(() => {
     const audio = audioRef.current;
@@ -100,7 +110,7 @@ export default function AudioPlayer({
             </div>
           </div>
         </div>
-        <div className="absolute flex items-end justify-end h-[5%] z-20 border w-full">
+        <div className="absolute flex items-end justify-end h-[5%] z-20 w-full">
           <button
             onClick={() => setIsMoreOpen(!isMoreOpen)}
             className="bg-white h-auto border border-l-black font-bold text-lg text-black flex items-center justify-center p-2.5 w-[10%]"
@@ -119,9 +129,9 @@ export default function AudioPlayer({
           </button>
         </div>
       </div>
-      <div className="flex  w-full bg-white items-center pr-2 py-0">
+      <div className="flex w-full bg-white items-center pr-2 py-0">
         <audio ref={audioRef} src={episode.audioUrl} className="hidden" />
-        <div className="w-1/6 md:w-[10%] lg:w-[5%] aspect-square relative overflow-hidden">
+        <div className="w-1/4 md:w-[10%] lg:w-[5%] aspect-square relative overflow-hidden">
           <Image
             src={episode.imageUrl}
             alt="Image of episode"
@@ -130,15 +140,16 @@ export default function AudioPlayer({
             className="object-cover"
           />
         </div>
-        <button onClick={togglePlay} className="mx-4">
-          {isPlaying ? (
+        <button onClick={togglePlay} className="mx-4" disabled={isLoading}>
+          {isLoading ? (
+            <Loader className="text-black animate-spin" />
+          ) : isPlaying ? (
             <Pause className="text-black" />
           ) : (
             <Play className="text-black" />
           )}
         </button>
         <div className="text-xs text-black mr-2">{formatTime(currentTime)}</div>
-
         <input
           type="range"
           min={0}
@@ -146,6 +157,7 @@ export default function AudioPlayer({
           value={currentTime}
           onChange={handleSeek}
           className="w-full h-1 bg-black rounded-full appearance-none cursor-pointer range-sm accent-black"
+          disabled={isLoading}
         />
         <div className="ml-2 text-black text-xs">{formatTime(duration)}</div>
       </div>
