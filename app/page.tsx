1e53874bb3ba37e7dcefda65ca4afadeee17c571
ndela-ntu <ntulilindelani4@gmail.com>
2024-10-16 19:37:42 +0200

    limit 5 and order

--- app/page.tsx
+++ app/page.tsx
@@ -23,14 +23,18 @@ export default async function Page() {
   const { data: shopItems, error: shopError } = await supabase
     .from("shop_items")
     .select("*")
+    .order("createdAt", { ascending: false })
+    .limit(5)
     .abortSignal(AbortSignal.timeout(5000));
 
   const { data: episodes, error: episodesError } = await supabase
     .from("episodes")
     .select("*")
+    .order("createdAt", { ascending: false })
+    .limit(5)
     .abortSignal(AbortSignal.timeout(5000));
 
-  const response = await client.getEntries({ content_type: "blogPost" });
+  const response = (await client.getEntries({ content_type: "blogPost", order: ['-sys.createdAt'], limit: 5}));
   const blogPosts: IBlogPost[] = response.items.map((item: any) => {
     const { content, slug, title, image, author, date } = item.fields;
 
