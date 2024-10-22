d848a3642e5c8bc32b3ddbf4415ba033d3fbacc9
ndela-ntu <ntulilindelani4@gmail.com>
2024-10-22 19:57:41 +0200

    updated

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -1,15 +1,16 @@
 export default function Page() {
   return (
     <div>
-      Founded in 2024 and based in Freedom Park, Soweto, SXNICS is dedicated to
-      bringing you the freshest underground tunes. We showcase a wide range of
-      genres, including deep house, lounge, ambient, and more, with carefully
-      selected weekly releases curated by our passionate music experts.
-      Alongside these unique tracks, we feature exclusive DJ episodes and
-      occasional interviews with artists who are making waves in the scenes. As
-      we grow, we aim to expand our library to include licensed music, funded
-      through our online store and monthly subscription plans, ensuring you
-      continue to enjoy the best music, legally and uninterrupted.
+      Your underground music stop. Founded in 2024 and based in Freedom Park,
+      Soweto, SXNICS is dedicated to bringing you the freshest underground
+      tunes. We showcase a wide range of genres, including deep house, lounge,
+      ambient, and more, with carefully selected weekly releases curated by our
+      passionate music experts. Alongside these unique tracks, we feature
+      exclusive DJ episodes and occasional interviews with artists who are
+      making waves in the scenes. As we grow, we aim to expand our library to
+      include licensed music, funded through our online store and monthly
+      subscription plans, ensuring you continue to enjoy the best music, legally
+      and uninterrupted.
     </div>
   );
 }
