bf99b4e4e1fbe1b439607641690f60f623c22b5d
ndela-ntu <ntulilindelani4@gmail.com>
2025-06-09 20:25:22 +0200

    hmm

--- components/EventsCarousel.tsx
+++ components/EventsCarousel.tsx
@@ -1,17 +1,8 @@
 "use client";
 
-import { IRelease } from "@/models/Release";
-import { Card, CardContent, CardTitle } from "./ui/card";
-import {
-  Carousel,
-  CarouselApi,
-  CarouselContent,
-  CarouselItem,
-} from "./ui/carousel";
 import Image from "next/image";
 import Link from "next/link";
-import Autoplay from "embla-carousel-autoplay";
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { IEvent } from "@/models/Event";
 import { formatDate } from "@/lib/datetime-formatter";
 
@@ -48,14 +39,6 @@ export default function EventsCarousel({
     }
   }, [activeTab]);
 
-  if (events.length === 0) {
-    return (
-      <div className="w-full flex items-center">
-        <span className="text-sm md:text-base">No events to show</span>
-      </div>
-    );
-  }
-
   return (
     <div className="flex flex-col py-2.5 space-y-2.5">
       <div className="text-semibold text-sm lg:text-lg w-full flex items-center justify-center space-x-2.5 lg:space-x-5">
@@ -90,36 +73,42 @@ export default function EventsCarousel({
           Past Events
         </button>
       </div>
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2.5 md:gap-5">
-        {events.map((event) => (
-          <div key={event.id}>
-            <div className="border flex justify-between py-2 px-2 space-x-5">
-              <span className="text-xs font-bold truncate">{event.name}</span>
-              <span className="text-xs">
-                {formatDate(new Date(event.eventDate))}
-              </span>
-            </div>
-            <div className="flex bg-transparent aspect-square items-center justify-center p-0">
-              <div className="w-full h-full">
-                <div className="aspect-square relative overflow-hidden">
-                  <Link href={`events/${event.id}`}>
-                    <Image
-                      src={event.coverUrl}
-                      alt="Image of episode"
-                      fill
-                      sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                      className="object-cover"
-                    />
-                  </Link>
-                  <span className="flex items-center justify-center text-sm px-1 h-7 absolute bottom-0 left-0 bg-black/50 hover:bg-black/70 text-white">
-                    {event.location}
-                  </span>
+      {events.length > 0 ? (
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2.5 md:gap-5">
+          {events.map((event) => (
+            <div key={event.id}>
+              <div className="border flex justify-between py-2 px-2 space-x-5">
+                <span className="text-xs font-bold truncate">{event.name}</span>
+                <span className="text-xs">
+                  {formatDate(new Date(event.eventDate))}
+                </span>
+              </div>
+              <div className="flex bg-transparent aspect-square items-center justify-center p-0">
+                <div className="w-full h-full">
+                  <div className="aspect-square relative overflow-hidden">
+                    <Link href={`events/${event.id}`}>
+                      <Image
+                        src={event.coverUrl}
+                        alt="Image of episode"
+                        fill
+                        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                        className="object-cover"
+                      />
+                    </Link>
+                    <span className="flex items-center justify-center text-sm px-1 h-7 absolute bottom-0 left-0 bg-black/50 hover:bg-black/70 text-white">
+                      {event.location}
+                    </span>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
+      ) : (
+        <div className="w-full flex items-center">
+          <span className="text-sm md:text-base">No events to show</span>
+        </div>
+      )}
     </div>
   );
 }
